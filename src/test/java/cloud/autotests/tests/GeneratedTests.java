9d005b12a47eae27708c7db12cbdb2ea20b55faa
YuryLoskutov <megafudziyama@gmail.com>
2022-05-30 22:23:25 +0300

    Added some raw tests

--- src/test/java/cloud/autotests/tests/GeneratedTests.java
+++ src/test/java/cloud/autotests/tests/GeneratedTests.java
@@ -10,8 +10,6 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
 
-//
-
 public class GeneratedTests extends TestBase {
     @Test
     @Description("Проверка")
