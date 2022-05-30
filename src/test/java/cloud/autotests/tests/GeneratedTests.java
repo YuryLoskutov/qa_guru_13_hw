e66253cb5b6c3caafb079d6002bc5d3ef9cde19d
YuryLoskutov <megafudziyama@gmail.com>
2022-05-30 22:04:28 +0300

    Added some raw tests

--- src/test/java/cloud/autotests/tests/GeneratedTests.java
+++ src/test/java/cloud/autotests/tests/GeneratedTests.java
@@ -1,47 +1,47 @@
 package cloud.autotests.tests;
 
 import cloud.autotests.helpers.DriverUtils;
+import com.codeborne.selenide.Condition;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.title;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class GeneratedTests extends TestBase {
     @Test
-    @Description("Soon to be implemented by you (or QA.GURU engineers)")
+    @Description("Проверка")
     @DisplayName("DTF_tests")
     void generatedTest() {
         step("Open dtf.ru", () -> {
-            step("// todo: just add selenium action");
+            open("https://dtf.ru/");
         });
 
         step("Check right panel availability", () -> {
-            step("// todo: just add selenium action");
-        });
-
-        step("Open Популярное tab", () -> {
-            step("// todo: just add selenium action");
+            $(".layout__left-column").shouldBe(Condition.exist);
         });
 
         step("Open Свежее tab", () -> {
-            step("// todo: just add selenium action");
+            $(".sidebar-tree-list-item", 1).click();
+            $(".feed__chunk").shouldBe(Condition.exist);
         });
 
         step("Open Вакансии tab", () -> {
-            step("// todo: just add selenium action");
+            $(".sidebar-tree-list-item", 2).click();
+            $(".v-header__title").shouldHave(Condition.text("Вакансии"));
         });
 
-        step("Open рейтинг DTF tab", () -> {
-            step("// todo: just add selenium action");
+        step("Open Рейтинг DTF tab", () -> {
+            $(".sidebar-tree-list-item", 3).click();
+            $(".subsite_head__name").shouldHave(Condition.text("Рейтинг сообществ и блогов"));
         });
 
-        step("Open подписки tab", () -> {
-            step("// todo: just add selenium action");
+        step("Open Подписки tab", () -> {
+            $(".sidebar-tree-list-item", 4).click();
+            $(".ui-tabs__content").shouldHave(Condition.text("Подписки"));
         });
     }
 }
